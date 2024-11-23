e5ef3bcc1c302cfb38da2b6c74a02cf81a6775b7
rithika74 <rithuvj22@gmail.com>
2024-11-23 22:21:50 +0530

    ....

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Context from "./services/Context.jsx";
 import {  HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter basename="/rbac">
+  <HashRouter basename="/rbac/">
     <Context>
       <App />
     </Context>
