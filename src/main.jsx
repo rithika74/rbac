dfac78fb6151ee05d740f9de7baa08185116d334
rithika74 <rithuvj22@gmail.com>
2024-11-23 21:22:15 +0530

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Context from "./services/Context.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";  // Import HashRouter
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/rbac/">
+  <HashRouter basename="/rbac/"> {/* Use HashRouter instead of BrowserRouter */}
     <Context>
       <App />
     </Context>
-  </BrowserRouter>
+  </HashRouter>
 );
