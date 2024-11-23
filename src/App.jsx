67a4431a7f78bd8ccd231e0027321f293cc4a180
rithika74 <rithuvj22@gmail.com>
2024-11-23 22:11:37 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-// App.js
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import RouterConnection from "./connection/RouterConnection";
 import PageLogin from "./pages/public/PageLogin";
 import PageDashboard from "./pages/private/Dasboard/PageDashboard";
 import Table from "./components/Table";
 import PageNotFound from "./pages/public/PageNotFound";
-import { usersPath, welcomePath } from "./services/UrlPaths";
+// import { PaginationProvider } from "./contexts/PaginationContext";
+import {
+  basePath,
+  usersPath,
+  welcomePath,
+} from "./services/UrlPaths";
 import UsersList from "./pages/private/Users/UsersList";
 import PrivateRoute from "./utils/PrivateRoute";
 import Welcome from "./pages/private/Welcome/Welcome";
@@ -14,14 +18,16 @@ import Welcome from "./pages/private/Welcome/Welcome";
 function App() {
   return (
     <div>
+
       <Routes>
         <Route path="/login" element={<PageLogin />} />
-        <Route path="/" element={<PrivateRoute><RouterConnection /></PrivateRoute>}>
-          <Route index path={welcomePath} element={<Welcome />} />
-          <Route path={usersPath} element={<UsersList />} />
+        <Route path={basePath} element={<PrivateRoute><RouterConnection /></PrivateRoute>}>
+          <Route index path={basePath + welcomePath} element={<Welcome />} />
+          <Route path={basePath + usersPath} element={<UsersList />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+
     </div>
   );
 }
