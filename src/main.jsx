6ec8406fa2535293b9a1f473dfddd4fd0c41bfa5
rithika74 <rithuvj22@gmail.com>
2024-11-23 21:06:53 +0530

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Context from "./services/Context.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/rbac/">
     <Context>
       <App />
     </Context>
