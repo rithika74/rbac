9904baa9a8aeac07959fec394b793babedbce626
rithika74 <rithuvj22@gmail.com>
2024-11-23 22:27:49 +0530

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import RouterConnection from "./connection/RouterConnection";
 import PageLogin from "./pages/public/PageLogin";
-import PageDashboard from "./pages/private/Dasboard/PageDashboard";
-import Table from "./components/Table";
 import PageNotFound from "./pages/public/PageNotFound";
-// import { PaginationProvider } from "./contexts/PaginationContext";
-import {
-  basePath,
-  usersPath,
-  welcomePath,
-} from "./services/UrlPaths";
+import { basePath, usersPath, welcomePath } from "./services/UrlPaths";
 import UsersList from "./pages/private/Users/UsersList";
 import PrivateRoute from "./utils/PrivateRoute";
 import Welcome from "./pages/private/Welcome/Welcome";
@@ -18,16 +11,14 @@ import Welcome from "./pages/private/Welcome/Welcome";
 function App() {
   return (
     <div>
-
       <Routes>
         <Route path="/login" element={<PageLogin />} />
-        <Route path={basePath} element={<PrivateRoute><RouterConnection /></PrivateRoute>}>
-          <Route index path={basePath + welcomePath} element={<Welcome />} />
-          <Route path={basePath + usersPath} element={<UsersList />} />
+        <Route path={`${basePath}`} element={<PrivateRoute><RouterConnection /></PrivateRoute>}>
+          <Route index path={`${basePath}${welcomePath}`} element={<Welcome />} />
+          <Route path={`${basePath}${usersPath}`} element={<UsersList />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Context from "./services/Context.jsx";
-import {  HashRouter } from "react-router-dom";
+import {  BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter basename="/rbac/">
+  <BrowserRouter basename="/rbac/">
     <Context>
       <App />
     </Context>
-  </HashRouter>
+  </BrowserRouter>
 );
